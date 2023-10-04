41126a3f90379878929f345fe0223722bffb26de
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2023-10-04 21:32:32 +0100

    new

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,47 +1,20 @@
 import React from 'react'
 import '../App.css'
+import { PaperPlaneRight } from '@phosphor-icons/react'
 
 export default function TodoForm() {
   return (
     <>
-        <div className="w-screen h-full bg-[url(../public/img2.jpg)]">
-            <div className="todo-container bg-slate-600 bg-opacity-[95%] text-white h-screen w-[90%] mx-[5%] md:w-[70%] mx-auto">
-
-              <div className="title text-center flex justify-center align-middle">
-                <h1>Todo List</h1>
-              </div>
-              <br />
-              <hr />
-              <br />
-
-              <div className="todo-wrapper block px-10 w-full md:flex lg:flex ">
-                <div className="title-input mr-[100px]">
-                  <label htmlFor="title" className='mr-3'>Title</label>
-                  <input type="text" />
-                </div>
-
-                <div className="description mr-5">
-                  <label htmlFor="description" className='mr-5'>Description</label>
-                  <input type="text" className=' w-[300px] ' />
-                </div>
-
-                <button type="submit" className='px-4 bg-slate-50 text-slate-700'>Add</button>
-              </div>
-              <br />
-              <hr />
-              <hr />
-
-              <div className="content mt-5 px-10">
-                Task 1
-              </div>
-
-              <div className="general-btn mt-5 px-10">
-                <button className='px-4 bg-slate-50 text-slate-700 mr-5'>Todo</button>
-                <button className='px-4 bg-slate-50 text-slate-700'>Complete</button>
-              </div>
-
-            </div>
+      <div className="main-container">
+        <div className="top"></div>
+        <div className="background w-screen absolute top-[180px]">
+          <div className="top-bg w-full relative" >
+            <div className='w-[80%] mx-[10%] md:w-[70%] lg:w-[70%]'><label htmlFor="" className='text-white'>Task</label></div>
+            <input type="text" placeholder='Your Task' className='w-[80%] mx-[10%] px-[3%] md:w-[80%]  lg:w-[80%]' />
+            <button className=' absolute left-[82%] text-white p-[10px] rounded-r-[10px] bg-slate-950 md:left-[88%] '><PaperPlaneRight size={20} /></button>
+          </div>
         </div>
+      </div>
     </>
   )
 }
